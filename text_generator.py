436e2502404aa7b8548476c38622d0c3297d4a8d
D.Stepanov <94580192+dmitrystepanov2@users.noreply.github.com>
2023-01-14 15:53:03 +0300

    Update text_generator.py
    small code refactor

--- text_generator.py
+++ text_generator.py
@@ -7,59 +7,59 @@ import re
 heads_dict = defaultdict(list)
 bigrams = list()
 
-def FileRead(name):
+
+def file_read(name):
     global bigrams
     file = open(name, 'r', encoding="utf-8")
     text = file.read()
     file.close()
     bigrams = nltk.WhitespaceTokenizer().tokenize(text)
-    #bigrams = list(nltk.bigrams(bigrams))
     bigrams = list(nltk.ngrams(bigrams, 3))
     for ram in bigrams:
         heads_dict[f"{ram[0]} {ram[1]}"].append(ram[2])
-def Markov(head):
+
+
+def markov_func(head):
     global heads_dict
     a = Counter(heads_dict[head]).most_common()
-    next_word = a[0]
-    rr = next_word[0]
-    a =head.split()
-    key = a[-1] + ' ' + rr
+    next_word = a[0][0]
+    key = head.split()[-1] + ' ' + next_word
     key = key.split()
     return key
 
-def ChoiseHead():
+
+def choise_head():
     global heads_dict
     heads = list(heads_dict.keys())
     head = random.choices(heads)
     return head[0]
 
-def HeadUp():
+
+def head_up():
     while True:
-        head = ChoiseHead()
-        if bool(re.match('^[A-Z]', head)) and not bool(re.match('[A-Za-z|a-z]+[\.|\?|\!]', head)): #and not bool(re.match('[\.|\?|\!]$', head)):
+        head = choise_head()
+        if bool(re.match('^[A-Z]', head)) and not bool(
+                re.match('[A-Za-z|a-z]+[\.|\?|\!]', head)):  # and not bool(re.match('[\.|\?|\!]$', head)):
             break
     return head
 
-def main():
 
-    file_name = input()
-    FileRead(file_name)
+def main():
+    file_name = input('введите имя файла исходного текста:')
+    file_read(file_name)
     for _ in range(10):
         sentences = 2
-        head = HeadUp()
+        head = head_up()
         string = head + ' '
         while True:
-            sss = Markov(head)
-            string += sss[-1] + ' '
+            markov = markov_func(head)
+            string += markov[-1] + ' '
             sentences += 1
-            if bool(re.findall('[\.|\?|\!]', sss[-1])):
+            if bool(re.findall('[\.|\?|\!]', markov[-1])):
                 if sentences >= 5:
                     break
-            head = sss[0] + ' ' + sss[-1]
+            head = markov[0] + ' ' + markov[-1]
         print(f"{string}")
-
-
-
 
 
 if __name__ == "__main__":
